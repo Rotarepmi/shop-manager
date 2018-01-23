0c9f8ed548db6fa9548461b17960850bd4ba6f1f
Jakub Mandra <jak_man@wp.pl>
2018-01-23 19:17:00 +0100

    add product form built

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       warehouse: [],
       sale: [],
       sold: []
-    }
+    };
+    this.addProduct = this.addProduct.bind(this);
   }
 
   componentDidMount() {
@@ -22,83 +23,101 @@ class App extends Component {
       });
   }
 
+  addProduct(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch('/products/addProd', {
+      method: 'POST',
+      body: data,
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Produkty</h1>
-        <div className="Container">
+      <styles.MainDiv>
+        <h1>Zarządzaj produktami w sklepie</h1>
 
-          <form>
-            <input type="text" placeholder="Kod produktu" required></input>
-            <input type="text" placeholder="Nazwa produktu" required></input>
-            <input type="text" placeholder="Producent" required></input>
-            <input type="text" placeholder="Kategoria" required></input>
-            <input type="text" placeholder="Cena produktu" required></input>
-            <input type="text" placeholder="Kod produktu" required></input>
-          </form>
+        <h2>Dodaj produkt</h2>
 
-          <styles.Table>
-            <tbody>
-              <styles.TableRow>
-                <styles.TableHeader>
-                  Kod
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Nazwa
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Producent
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Kategoria
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Cena (z obniżką)
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Obniżka
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Magazyn
-                </styles.TableHeader>
-                <styles.TableHeader>
-                  Sprzedano
-                </styles.TableHeader>
+        <form onSubmit={this.addProduct}>
+          <styles.Input type="number" name="code" placeholder="Kod produktu" required></styles.Input>
+          <styles.Input type="text" name="name" placeholder="Nazwa produktu" required></styles.Input>
+          <styles.Input type="text" name="manufacturer" placeholder="Producent" required></styles.Input>
+          <styles.Input type="text" name="category" placeholder="Kategoria" required></styles.Input>
+          <styles.Input type="number" name="price" placeholder="Cena produktu" required></styles.Input>
+          <styles.Input type="number" name="am" placeholder="Ilość" required></styles.Input>
+
+          <button>Dodaj produkt</button>
+        </form>
+
+        <h2>Lista produktów</h2>
+        <styles.Table>
+          <tbody>
+            <styles.TableRow>
+              <styles.TableHeader>
+                Kod
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Nazwa
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Producent
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Kategoria
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Cena (z obniżką)
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Obniżka
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Magazyn
+              </styles.TableHeader>
+              <styles.TableHeader>
+                Sprzedano
+              </styles.TableHeader>
+            </styles.TableRow>
+
+            {this.state.products.map(product =>
+              <styles.TableRow key={product._id}>
+                <styles.TableData>
+                  {product.code}
+                </styles.TableData>
+                <styles.TableData>
+                  {product.name}
+                </styles.TableData>
+                <styles.TableData>
+                  {product.manufacturer}
+                </styles.TableData>
+                <styles.TableData>
+                  {product.category}
+                </styles.TableData>
+                <styles.TableData>
+                  {product.prod_sale.length > 0 ? (Math.round((product.prod_price[0].price * (100 - product.prod_sale[0].reduction))) / 100).toFixed(2) : product.prod_price[0].price}zł
+                </styles.TableData>
+                <styles.TableData>
+                  {product.prod_sale.length > 0 ? product.prod_sale[0].reduction : '--'}%
+                </styles.TableData>
+                <styles.TableData>
+                  {product.prod_ware[0].am}
+                </styles.TableData>
+                <styles.TableData>
+                  {product.prod_sold.length > 0 ? product.prod_sold[0].amount : '--'}
+                </styles.TableData>
               </styles.TableRow>
+            )}
 
-              {this.state.products.map(product =>
-                <styles.TableRow key={product._id}>
-                  <styles.TableData>
-                    {product.code}
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.name}
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.manufacturer}
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.category}
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.prod_sale.length > 0 ? (Math.round((product.prod_price[0].price * (100 - product.prod_sale[0].reduction))) / 100).toFixed(2) : product.prod_price[0].price}zł
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.prod_sale.length > 0 ? product.prod_sale[0].reduction : '--'}%
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.prod_ware[0].am}
-                  </styles.TableData>
-                  <styles.TableData>
-                    {product.prod_sold.length > 0 ? product.prod_sold[0].amount : '--'}
-                  </styles.TableData>
-                </styles.TableRow>
-              )}
+          </tbody>
+        </styles.Table>
 
-            </tbody>
-          </styles.Table>
-        </div>
-      </div>
+      </styles.MainDiv>
     );
   }
 }
